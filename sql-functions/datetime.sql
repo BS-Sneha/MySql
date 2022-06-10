7df978bbd5929ba57aade93683ff0475455f0ca9
BS-Sneha <sneha.sridhar@radiantcloud.co>
2022-06-10 14:50:54 +0530

    Date and time using Mysql

---
+++ sql-functions/datetime.sql
@@ -0,0 +1,6 @@
+-- create table users (name varchar(30),
+-- col1 date,col2 time ,col3 datetime);
+
+-- insert into users(name,col1,col2,col3)values("hitesh",'2021-01-01','10:14:22',NOW());
+
+-- select name, DATE_FORMAT(col3,'%m/%d/%Y') from users;
